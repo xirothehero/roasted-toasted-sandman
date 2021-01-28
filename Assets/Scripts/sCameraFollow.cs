e0e9bd13316b0ae49ca916ea9f8b2e46f446e3ea
DylanS2 <61853391+DylanS2@users.noreply.github.com>
2021-01-28 01:29:05 -0600

    Character controller with camera follow

---
+++ Assets/Scripts/sCameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sCameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float turnSpeed = 8;
+    public float yPosOffset = 3;
+
+    private Vector3 offset;
+    
+    private void Start()
+    {
+        offset = new Vector3(player.position.x, player.position.y + yPosOffset, player.position.z + Vector3.Distance(transform.position, player.position));
+    }
+    void Update()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X")*turnSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
---
+++ Assets/Scripts/sPlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sPlayer : MonoBehaviour
+{
+    public float health = 100;
+    public float speed = 8;
+    public Transform theCamera;
+    float heading = 0;
+    float keep = 0;
+    void Update()
+    {
+        float zInput = Input.GetAxis("Vertical");
+        float xInput = Input.GetAxis("Horizontal");
+
+
+        Rotate(xInput, zInput);
+
+        if (zInput != 0 || xInput != 0)
+        {
+            if (zInput == 1 || zInput == -1)
+                keep = zInput;
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+    }
+
+    void Rotate(float _xInput, float _zInput)
+    {
+        // Forward direction: camPos -> playerPos\\
+        Vector3 cameraVector = new Vector3(transform.position.x - Camera.main.transform.position.x, 0.0f,
+                                                   transform.position.z - Camera.main.transform.position.z);
+
+        // Calculate the look direction of the player based on the input and the cameraVector
+        Vector3 playerLookDirection = Quaternion.LookRotation(cameraVector) * new Vector3(_xInput, 0.0f, _zInput);
+
+        if (playerLookDirection != Vector3.zero)
+        {
+            Quaternion destRot = Quaternion.LookRotation(playerLookDirection);
+            transform.rotation = destRot;
+        }
+        else
+        {
+            if (keep == 1)
+            {
+                transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, theCamera.localEulerAngles.y, transform.localEulerAngles.z);
+            }
+            else
+            {
+                Vector3 lookPos = theCamera.position - transform.position;
+                lookPos.y = 0;
+                Quaternion rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = rotation;
+            }
+        }
+    }
+}

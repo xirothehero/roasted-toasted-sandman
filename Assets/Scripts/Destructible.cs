e11d8469e31814160a011f6638a801b454b9ccb1
justin2jam <32021869+justin2jam@users.noreply.github.com>
2021-01-31 04:18:53 -0600

    Added destructible box
    still wip

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    public GameObject brokenObj;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            gameObject.SetActive(false);
+            Instantiate(brokenObj, transform.position, transform.rotation);
+            print("FRACTURE!");
+        }
+    }
+
+}

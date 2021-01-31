76e9336dcb44c54db1bde178363b68fa8a070341
justin2jam <32021869+justin2jam@users.noreply.github.com>
2021-01-30 21:54:53 -0600

    Added some prefabs and hit fx

---
+++ Assets/Scripts/HitFX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitFX : MonoBehaviour
+{
+    public GameObject ps;
+    void PlayHitFX() {
+        ps.SetActive(false);
+        ps.SetActive(true);
+        
+    }
+}
